9a2309440bfa270cc8e9589f79294654db25b5bb
clicktronix <s28.morgan@gmail.com>
2016-01-19 16:24:25 +0600

    Добавлены модульные тесты API интвентора

---
+++ GasketModelGUI/UnitTests/InventorApiTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using NUnit.Framework;
+using GasketModelGUI;
+using Inventor;
+
+namespace UnitTests
+{
+    public class InventorAPITest
+    {
+        private InventorApi _inventorApi = new InventorApi();
+
+        [Test, Description("Позитивная инициализация")]
+        public void InitPositive()
+        {
+            //Подаем ссылку на приложение
+            Assert.DoesNotThrow(() => _inventorApi.Initialization(_inventorApi.InventorApplication));
+
+            //Очищаем документ
+            foreach (Document document in _inventorApi.InventorApplication.Documents)
+            {
+                document.Close(true);
+            }
+        }
+
+        [Test, Description("Негативная инициализация")]
+        public void InitNegative()
+        {
+            //Подаем ссылку на приложение
+            Assert.Throws<AccessingNullException>(() => _inventorApi.Initialization(null));
+
+            //Очищаем документ
+            foreach (Document document in _inventorApi.InventorApplication.Documents)
+            {
+                document.Close(true);
+            }
+        }
+    }
+}
