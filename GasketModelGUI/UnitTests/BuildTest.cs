0e2e82930404fb6efb5d2319a8c69dac7e4d9b93
clicktronix <s28.morgan@gmail.com>
2016-01-19 16:24:44 +0600

    Добавлены модульные тесты построения детали

---
+++ GasketModelGUI/UnitTests/BuildTest.cs
@@ -0,0 +1,52 @@
+﻿//using System;
+//using NUnit.Framework;
+//using GasketModelGUI;
+//using Inventor;
+
+//namespace UnitTests
+//{
+//    class BuildTest
+//    {
+//        private InventorApi _inventorApi = new InventorApi();
+//        private GasketModelCreator _gasketModelCreator;
+//        private GasketProperties _gasketProperties;
+
+//        [Test, Description("Позитивное построение")]
+//        public void BuildHandlebarPositive(GasketModelCreator gasketModelCreator, GasketProperties gasketProperties)
+//        {
+//            _gasketProperties = gasketProperties;
+//            _gasketModelCreator = gasketModelCreator;
+
+//            //Проверяем, что постройка не выкинула ексепшн.
+//            Assert.DoesNotThrow(() => _gasketModelCreator.Build(gasketProperties));
+
+//            //Очищаем документ
+//            foreach (Document document in _inventorApi.InventorApplication.Documents)
+//            {
+//                document.Close(true);
+//            }
+//        }
+//        [Test, Description("Позитивный тест для конструктора")]
+//        public void PartBuilderPositive()
+//        {
+//            //Проверяем конструктор валидной ссылкой.
+//            // ReSharper disable once ObjectCreationAsStatement
+//            Assert.DoesNotThrow(() => { new GasketModelCreator(_inventorApi.InventorApplication); });
+
+//            //подчищаем за собой
+//            foreach (Document document in _inventorApi.InventorApplication.Documents)
+//            {
+//                document.Close(true);
+//            }
+
+//        }
+
+//        [Test, Description("Негативный тест для конструктора")]
+//        public void PartBuilderNegative()
+//        {
+//            //Подаем невалидную ссылку на приложение.
+//            // ReSharper disable once ObjectCreationAsStatement
+//            Assert.Throws<AccessingNullException>(() => { new GasketModelCreator(null); });
+//        }
+//    }
+//}
